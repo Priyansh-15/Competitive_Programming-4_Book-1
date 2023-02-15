8d849bfea005479afb4206f2c31660b04aa6e8f9
Priyansh Sharma <77832407+Priyansh-15@users.noreply.github.com>
2023-02-15 15:28:48 +0530

    Update 3.cpp

--- 2/2.2/1/3.cpp
+++ 2/2.2/1/3.cpp
@@ -64,3 +64,60 @@ int main()
         sol();
     return 0;
 }
+
+// Code 2:
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+int sol()
+{
+    int n;
+    cin>>n;
+    int v;
+    cin>>b;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    map<int,int>m;
+    for(int i=0;i<n;i++)
+    {
+        if(m[v-a[i]]!=0)
+        {
+            cout<<a[i]<<" "<<v-a[i]<<endl;
+            return 0;
+        }
+        m[a[i]]++;
+    }
+    cout<<"No Pairs Exist"<<endl;
+    return 0;
+}
+ 
+int main() 
+{
+    ll t;
+    cin>>t;
+    while(t--)
+        sol();
+    return 0;
+}
